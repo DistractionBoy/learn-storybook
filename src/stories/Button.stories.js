807826cec3297ff4c7a6c0d1f953bb16a76b11e5
Jason Pierce - AMD <adm-jpierce@wrbts.ads.wrberkley.com>
2020-09-13 11:26:00 -0400

    fix(button stories): updating the import statement after making button component default export

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withDesign } from "storybook-addon-designs";
-import { Button } from "../components/Button";
+import Button from "../components/Button";
 
 export default {
   title: "Components/Button",
